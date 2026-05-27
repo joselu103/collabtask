6d35f793c174bebce14ed80e26de0a59f6502c3f
Joselu <joseluiscc103@gmail.com>
2026-05-27 09:37:01 +0200

    Implement register user

--- src/shared/router.py
+++ src/shared/router.py
@@ -3,11 +3,13 @@ from fastapi import APIRouter
 
 from src.health.router import router as health_router
 from src.organizations.router import router as org_router
+from src.users.router import router as user_router
 
 # V1 router
 v1_router = APIRouter(prefix="/v1")
 v1_router.include_router(health_router)
 v1_router.include_router(org_router)
+v1_router.include_router(user_router)
 
 # API router
 api_router = APIRouter(prefix="/api")
